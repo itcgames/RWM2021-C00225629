dd71243779945fd6efbc7beb4a9d12538bfdbdbd
KrystianSarowski <C00225629@itcarlow.ie>
2020-11-14 18:08:23 +0000

    JV-3 #2h 27m Map object created, tile grid can be created in map and dimensions can be set #Done

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
+{
+    int m_width = 0;
+    int m_hight = 0;
+
+    List<List<Tile>> m_mapGrid = new List<List<Tile>>();
+
+    public void SetSize(int t_width, int t_hight)
+    {
+        m_width = t_width;
+        m_hight = t_hight;
+    }
+
+    public void SetWidth(int t_width)
+    {
+        m_width = t_width;
+    }
+
+    public void SetHight(int t_hight)
+    {
+        m_hight = t_hight;
+    }
+
+    public int GetWidth()
+    {
+        return m_width;
+    }
+
+    public int GetHight()
+    {
+        return m_hight;
+    }
+
+    public void CreateMap()
+    {
+        if(m_mapGrid.Count == 0)
+        {
+            for (int x = 0; x < m_width; x++)
+            {
+                List<Tile> col = new List<Tile>();
+
+                for (int y = 0; y < m_hight; y++)
+                {
+                    Tile newTile = new Tile();
+                    newTile.SetIndexPos(new Pair<int, int>(x, y));
+
+                    col.Add(newTile);
+                }
+                m_mapGrid.Add(col);
+            }
+        }
+    }
+
+    public int GetTileCount()
+    {
+        int count = 0;
+        foreach(List<Tile> col in m_mapGrid)
+        {
+            count += col.Count;
+        }
+        return count;
+    }
+}
---
+++ Assets/Tests/MapSuite.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapSuite
+{
+    Map m_map = null;
+
+    [SetUp]
+    public void Setup()
+    {
+        m_map = new Map();
+    }
+
+    [UnityTest]
+    public IEnumerator MapDimensions()
+    {
+        int width = 25;
+        int hight = 37;
+
+        m_map.SetSize(width, hight);
+
+        Assert.AreEqual(width, m_map.GetWidth());
+        Assert.AreEqual(hight, m_map.GetHight());
+
+        m_map.CreateMap();
+
+        Assert.AreEqual(width * hight, m_map.GetTileCount());
+
+        yield return new WaitForSeconds(0.1f);
+    }
+}
