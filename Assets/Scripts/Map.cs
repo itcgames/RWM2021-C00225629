a7ed15da2eedaceb04f7f2b1a2dba83669dd0e19
KrystianSarowski <C00225629@itcarlow.ie>
2020-11-27 14:09:12 +0000

    JV-9 #5h 20 min Added a check for detecting if the character has moved out of the map boundries #Done

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
-    public int m_width = 0;
-    public int m_hight = 0;
+    [Range(1, 1000)]
+    public int m_width = 0;                 //Number of tiles in a singal row of the map.
+
+    [Range(1, 1000)]
+    public int m_hight = 0;                 //Number of tiles in a single column of the map.
 
     List<List<Tile>> m_mapGrid = new List<List<Tile>>();
     List<GameObject> m_mapEnteties = new List<GameObject>();
 
-    public float tileSize = 0.32f;
-    public string m_charTag = "Character";
-    public string m_objectTag = "Object";
+    public float tileSize = 0.32f;          //The size of a singular tile in pixels.
+    public string m_charTag = "Character";  //The tag used for enteties that move around the map.
+    public string m_objectTag = "Object";   //The tag used for enteties that when placed in a tile prevent other enteties from ever being added to the tile.
 
     public void SetSize(int t_width, int t_hight)
     {
@@ -89,19 +92,22 @@ public class Map : MonoBehaviour
 
         foreach(GameObject entity in m_mapEnteties)
         {
-            if (entity.tag == "Character")
+            if (entity.tag == m_charTag)
             {
                 //List of the tiles the character was previously.
-                List<Pair<int, int>> previousTiles = GetTilesWithEntity(entity);
+                List<Pair<int, int>> prevOccupiedTiles = GetTilesWithEntity(entity);
 
-                //List of the tiles the character is going to be in now
-                List<Pair<int, int>> newTiles = new List<Pair<int, int>>();
+                //List of the tiles the character is going to be in now.
+                List<Pair<int, int>> newOccupiedTiles = new List<Pair<int, int>>();
 
                 //Collider of character used to check for change in tile.
                 RectangularCollider collider = entity.GetComponent<RectangularCollider>();
 
-                //Bool for if a collision has occured.
+                //Bool for if a collision has occured with a tile that can not be traversed.
                 bool collisionDetected = false;
+
+                //Bool for if a character has move out side the boundry that is unguarded.
+                bool outOfBounds = false;
 
                 //Check if the collider scripts exists on the object.
                 if (collider != null)
@@ -116,48 +122,77 @@ public class Map : MonoBehaviour
                     foreach (Pair<int, int> corner in cornerList)
                     {
                         cornerPos = collider.m_pos + new Vector2(corner.m_first * collider.m_width / 2, corner.m_second * collider.m_hight / 2);
+                        
                         cornerMapIndex = new Pair<int, int>((int)(cornerPos.x / tileSize), (int)(cornerPos.y / tileSize));
+
+                        //When position goes into negative it is assumed it goes to negative index number 
+                        //thus it needs to be adjusted.
+                        if(cornerPos.x < 0)
+                        {
+                            cornerMapIndex.m_first -= 1;
+                        }
+
+                        if(cornerPos.y < 0)
+                        {
+                            cornerMapIndex.m_second -= 1;
+                        }
 
                         //Check if we are not checking a tile that we are already in as that would make us be stuck in that tile for
                         //ever is something was spawned in that tile.
-                        if (!m_mapGrid[cornerMapIndex.m_first][cornerMapIndex.m_second].GetEntityList().Contains(entity))
+                        if(cornerMapIndex.m_first >= 0 && cornerMapIndex.m_first < m_width && 
+                            cornerMapIndex.m_second >= 0 && cornerMapIndex.m_second < m_hight)
                         {
-                            //Check if the tile is traversable.
-                            if (!m_mapGrid[cornerMapIndex.m_first][cornerMapIndex.m_second].GetIsTraversable())
+                            if (!m_mapGrid[cornerMapIndex.m_first][cornerMapIndex.m_second].GetEntityList().Contains(entity))
                             {
-                                //The tile is not traversable and thus collsion has been detected.
-                                collisionDetected = true;
+                                //Check if the tile is traversable.
+                                if (!m_mapGrid[cornerMapIndex.m_first][cornerMapIndex.m_second].GetIsTraversable())
+                                {
+                                    //The tile is not traversable and thus collsion has been detected.
+                                    collisionDetected = true;
+                                }
+                                else
+                                {
+                                    newOccupiedTiles.Add(new Pair<int, int>(cornerMapIndex.m_first, cornerMapIndex.m_second));
+                                }
                             }
                             else
                             {
-                                newTiles.Add(new Pair<int, int>(cornerMapIndex.m_first, cornerMapIndex.m_second));
+                                newOccupiedTiles.Add(new Pair<int, int>(cornerMapIndex.m_first, cornerMapIndex.m_second));
                             }
                         }
                         else
                         {
-                            newTiles.Add(new Pair<int, int>(cornerMapIndex.m_first, cornerMapIndex.m_second));
+                            outOfBounds = true;
+                            break;
                         }
+
                     }
 
                     //Check if the collision has been detected.
-                    if (!collisionDetected)
+                    if (!collisionDetected && !outOfBounds)
                     {
-                        foreach (Pair<int, int> indexPos in previousTiles)
+                        foreach (Pair<int, int> indexPos in prevOccupiedTiles)
                         {
-                            if (!newTiles.Contains(indexPos))
+                            if (!newOccupiedTiles.Contains(indexPos))
                             {
                                 m_mapGrid[indexPos.m_first][indexPos.m_second].DeleteEntity(entity);
                             }
                         }
 
-                        foreach (Pair<int, int> indexPos in newTiles)
+                        foreach (Pair<int, int> indexPos in newOccupiedTiles)
                         {
                             m_mapGrid[indexPos.m_first][indexPos.m_second].AddEntity(entity);
                         }
                     }
-                    else
+                    else if(outOfBounds)
                     {
-                        Debug.Log("Character Collision Detected");
+                        //Do something here as a response to the character eg move back.
+                        Debug.Log("Character out of bounds detected!");
+                    }
+                    else 
+                    {
+                        //Do something here as a response to the character eg move back.
+                        Debug.Log("Character Collision Detected!");
                     }
                 }
             }
